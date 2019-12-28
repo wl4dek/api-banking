cf42d36f5eb71d69a23166e1b0538036652c8ae8
Wladek <Wladek>
2019-12-28 01:55:08 -0300

    Deploy production to Heroku database config

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :api_banking, ApiBankingWeb.Endpoint,
   url: [scheme: "https", host: "api-banking.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
-  # cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
